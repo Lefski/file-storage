dee880fbc680fd9ffc6d713f346b1c49f13cbead
Georgnapr <georgnapr@gmail.com>
2025-11-16 18:31:32 +0300

    Небольшие изменения стилей

--- frontend_file_storage/src/components/board/Board.jsx
+++ frontend_file_storage/src/components/board/Board.jsx
@@ -3,6 +3,6 @@ import "./Board.css"
 
 export default function Board() {
   return (
-    <div class="board">Beeeeerd</div>
+    <div class="board"></div>
   )
 }
--- frontend_file_storage/src/components/ui_components/search-bar/searchComponent.jsx
+++ frontend_file_storage/src/components/ui_components/search-bar/searchComponent.jsx
@@ -1,36 +1,11 @@
-
+import './searchComponent.css'
 const SearchComponent = () => {
-  const searchStyles = {
-    container: {
-      width: '400px',
-      height: '30px',
-      borderRadius: '5px',
-      border: '1px solid black',
-      display: 'flex',
-      alignItems: 'center',
-      padding: '0 10px',
-      fontFamily: 'Arial, sans-serif',
-      backgroundColor: 'white'
-    },
-    icon: {
-      marginRight: '8px',
-      fontSize: '16px'
-    },
-    input: {
-      border: 'none',
-      outline: 'none',
-      fontSize: '14px',
-      width: '100%',
-      background: 'transparent'
-    }
-  };
-
   return (
-    <label style={searchStyles.container}>
-      <input 
+    <label class="search__container">
+      <input
+        class="search__input" 
         type="text" 
         placeholder="Поиск" 
-        style={searchStyles.input}
       />
     </label>
   );
